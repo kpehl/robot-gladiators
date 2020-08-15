4b841614050d484ea7d5929a39f0d22376fa841f
kpehl <30782087+kpehl@users.noreply.github.com>
2020-08-15 10:59:20 -0400

    modified the game.js file to prompt for a robot name and wrote the name to the browser console

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,19 @@
-window.alert("This is an alert! JavaScript is running!");
+// User prompt for robot's name
+
+var playerName = window.prompt("What is your robot's name?");
+
+// Write the robot's name to the console log
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// This is a demonstration of a math function
+console.log(10+10);
+// This demonstrates concatenation
+console.log("Our robot's name is " + playerName);
+
+// Fight Function
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
